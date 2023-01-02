380be2f9a2b40eddc6b891c5025d661b22327b75
Yannick Mueller <yannick.mueller@fusiva.ch>
2023-01-02 21:55:31 +0100

    add table view for access and fixed loading bug

--- src/app/share-popup/share-popup.component.ts
+++ src/app/share-popup/share-popup.component.ts
@@ -1,17 +1,19 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Store } from '@ngrx/store';
-import { share } from 'rxjs';
+import { identity, Observable, Observer, share, Subscription } from 'rxjs';
 import { AppUser } from '../model/appuser.model';
 import { Folder } from '../model/folder.model';
 import { AppuserService } from '../service/appuser.service';
-import { loadAppUser } from '../state/appUser.action';
+import { loadAppUsByUserId, loadAppUser } from '../state/appUser.action';
 import { selectAppUser } from '../state/appUser.selector';
 import { authReducer } from '../state/auth.reducer';
 import { selectAuthUser } from '../state/auth.selector';
 import { addError } from '../state/error.action';
 import { updateFolder } from '../state/folder.action';
+import { selectFolderById } from '../state/folder.selector';
 
 @Component({
   selector: 'app-share-popup',
@@ -21,15 +23,29 @@ import { updateFolder } from '../state/folder.action';
 export class SharePopupComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<SharePopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private store: Store) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private store: Store, private _snackBar: MatSnackBar) { }
 
   me?: AppUser = undefined;
 
+  displayedColumns: Array<string> = ['username', 'email', 'accessType', 'actions']
+
+  owner: AppUser = this.data.folder.owner as AppUser;
+  haveAccess: Array<string> = [];
+
+  updateAccessTypes() {
+    this.haveAccess = [];
+    this.data.folder.viewAccess.forEach((uid: string) => this.haveAccess.push(uid));
+    this.data.folder.writeAccess.forEach((uid: string) => this.haveAccess.push(uid));
+
+  }
+
   ngOnInit(): void {
     this.store.select(selectAuthUser).subscribe(me => this.me = me);
+    this.store.select(selectFolderById({ folderId: this.data.folder.id })).subscribe(folder => { this.data.folder = { ...folder }; this.updateAccessTypes() });
   }
 
   addToFolder(appUser: AppUser, mode: string) {
+
     if (appUser.id == this.me?.id) {
       this.store.dispatch(addError({ message: "You can't give yourself access" }));
       return;
@@ -48,20 +64,17 @@ export class SharePopupComponent implements OnInit {
       this.store.dispatch(addError({ message: "This user does already have read or write access" }));
       return;
     }
-
     if (mode === "read") {
-      folder.viewAccess = [...folder.viewAccess];
-      folder.viewAccess.push(appUser.id);
+      folder.viewAccess = [...folder.viewAccess!];
+      folder.viewAccess!.push(appUser.id);
     } else {
-
-      folder.writeAccess = [...folder.writeAccess];
-      folder.writeAccess.push(appUser.id);
+      folder.writeAccess = [...folder.writeAccess!];
+      folder.writeAccess!.push(appUser.id);
     }
 
     this.store.dispatch(updateFolder(folder));
 
 
-    console.log(appUser);
   }
 
   shareForm = new FormGroup({
@@ -69,9 +82,22 @@ export class SharePopupComponent implements OnInit {
     readWrite: new FormControl("", [Validators.required]),
   });
 
+
+  lazyLoad(appUsers: Array<AppUser>) {
+    let form = this.shareForm.getRawValue();
+    let alreadyLoadet = false; appUsers.forEach(appUser => { if (appUser.email == form.userNameOrEmail || appUser.userName == form.userNameOrEmail) alreadyLoadet = true; }); if (!alreadyLoadet) this.store.dispatch(loadAppUser({ emailOrUsername: form.userNameOrEmail! }));
+  }
+
   initAccessFlow() {
     let form = this.shareForm.getRawValue();
-    this.store.select(selectAppUser).subscribe(appUsers => appUsers.forEach(appUser => { if (appUser.email == form.userNameOrEmail || appUser.userName == form.userNameOrEmail) this.addToFolder(appUser, form.readWrite!) }));
-    this.store.dispatch(loadAppUser({ emailOrUsername: form.userNameOrEmail! }));
+    this.store.select(selectAppUser).subscribe(appUsers => { this.lazyLoad(appUsers); appUsers.forEach(appUser => { if (appUser.email == form.userNameOrEmail || appUser.userName == form.userNameOrEmail) this.addToFolder(appUser, form.readWrite!) }) });
+
+  }
+
+  removeAccess(uid: string) {
+    let origignalFolder = this.data.folder;
+    this.data.folder.writeAccess = this.data.folder.writeAccess.filter((elm: string) => elm !== uid);
+    this.data.folder.viewAccess = this.data.folder.viewAccess.filter((elm: string) => elm !== uid);
+    this.store.dispatch(updateFolder(this.data.folder))
   }
 }
